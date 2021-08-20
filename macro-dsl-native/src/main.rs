66bc32306f16c5af3e8db25a0eecbcc8384d3ebf
haved <krogstie.havard@gmail.com>
2021-08-20 23:36:18 +0200

    rust was fun, let's go for zig next :)

--- macro-dsl-native/src/main.rs
+++ macro-dsl-native/src/main.rs
@@ -1,3 +1,41 @@
+use beryllium::*;
+use gl33::*;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = SDL::init(InitFlags::Everything).expect("couldn't start SDL");
+    sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core)
+        .unwrap();
+    #[cfg(target_os = "macos")]
+    {
+        sdl.gl_set_attribute(SdlGlAttr::Flags, ContextFlag::ForwardCompatible)
+            .unwrap();
+    }
+    let win = sdl
+        .create_gl_window(
+            "Hello Window",
+            WindowPosition::Centered,
+            800,
+            600,
+            WindowFlags::Shown,
+        )
+        .expect("couldn't make a window and context");
+
+    unsafe {
+        let gl =
+            GlFns::load_from(&|c_char_ptr| win.get_proc_address(c_char_ptr as *const i8)).unwrap();
+        gl.ClearColor(0.2, 0.3, 0.3, 1.0);
+
+        'main_loop: loop {
+            while let Some(event) = sdl.poll_events().and_then(Result::ok) {
+                match event {
+                    Event::Quit(_) => break 'main_loop,
+                    _ => (),
+                }
+            }
+
+            win.swap_window();
+        }
+    }
 }
--- macro-dsl/src/lib.rs
+++ macro-dsl/src/lib.rs
@@ -1,28 +1,64 @@
 
-pub struct Editor {
+pub struct Marker {
+    pos: u32
+}
+
+pub struct Buffer {
     name: String,
-    content: String
+    content: String,
+    markers: Vec<Marker>
 }
 
-pub struct MacroDSLEnvironment {
-    editor: Editor,
-    macro_editor: Editor
-}
-
-impl Editor {
+impl Buffer {
     pub fn new(name: &str, content: &str) -> Self {
         Self {
             name: String::from(name),
-            content: String::from(content)
+            content: String::from(content),
+            markers: vec!()
         }
     }
+}
+
+pub struct Window {
+    buffer_index: u32,
+    marker_index: u32,
+    twin_marker_index: Option<u32>
+}
+
+impl Window {
+    pub fn new(buffer_index: u32, marker_index: u32) -> Self {
+        Self {
+            buffer_index,
+            marker_index,
+            twin_marker_index: None
+        }
+    }
+}
+
+pub struct MacroDSLEnvironment {
+    buffers: Vec<Buffer>,
+    windows: Vec<Window>,
+    macro_editor: Buffer,
+    width: u32,
+    height: u32
 }
 
 impl MacroDSLEnvironment {
     pub fn new() -> Self {
         Self {
-            editor: Editor::new("Main Buffer", "This is the editor"),
-            macro_editor: Editor::new("Macro", "# Your macro gets written here")
+            buffers: vec!(Buffer::new("Main Buffer", "This is the editor")),
+            windows: vec!(Window::new(0, 0)),
+            macro_editor: Buffer::new("Macro", "# Your macro gets written here"),
+            width: 0, height: 0
         }
+    }
+
+    pub fn render(&mut self) {
+
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.width = width;
+        self.height = height;
     }
 }
